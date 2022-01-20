e2fb5d789f39c24d328ad66230dbafda0c52498f
pondsuyeon <pondsuyeon@gmail.com>
2022-01-20 13:43:24 +0900

    feat : :sparkles: 아이디 중복 확인 API

--- backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -41,7 +41,12 @@ public class UserServiceImpl implements UserService {
 		return userRepository.save(user);
 	}
 
-   @Override
+	@Override
+	public boolean idDupplicated(String userId) {
+		return userRepository.existsByUserId(userId);
+	}
+
+	@Override
    public User getUserByUserId(String userId) {
       // 디비에 유저 정보 조회 (userId 를 통한 조회).
       User user = userRepository.findByUserId(userId).get();
--- backend/src/main/java/com/ssafy/db/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/UserRepository.java
@@ -13,4 +13,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     // 아래와 같이, Query Method 인터페이스(반환값, 메소드명, 인자) 정의를 하면 자동으로 Query Method 구현됨.
     Optional<User> findByUserId(String userId);
+    boolean existsByUserId(String userId);
 }
