228d8c764060a90a30f29ee511509f3387447e85
snowwhitewolf <whdgus1029@naver.com>
2022-01-20 14:34:05 +0900

    feat : :sparkles: 프로젝트 생성

---
+++ frontend_test/src/routes/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>moyobar 홈화면</p>
+    </>
+  )
+}
---
+++ frontend_test/src/routes/Signup.tsx
@@ -0,0 +1,8 @@
+export default function Signup() {
+  return (
+    <>
+      <h1>회원가입</h1>
+      
+    </>
+  )
+}
