af100795591372bf491f21dbe5424ef3ac40ae5a
snowwhitewolf <whdgus1029@naver.com>
2022-01-21 11:26:48 +0900

    [#S06P12D210-31]fix : :bug: 프론트엔드 프로젝트 폴더 삭제

--- frontend/src/views/main/store/actions.js
+++
@@ -1,9 +0,0 @@
-// API
-import $axios from 'axios'
-
-export function requestLogin ({ state }, payload) {
-  console.log('requestLogin', state, payload)
-  const url = '/auth/login'
-  let body = payload
-  return $axios.post(url, body)
-}
