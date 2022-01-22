f6baddb6d5c45ab7eb57c312013f9d809f10db09
snowwhitewolf <whdgus1029@naver.com>
2022-01-22 11:25:44 +0900

    [#S06P12D210-46] feat : :sparkles: 로비 화면 구성

---
+++ frontend_test/src/components/lobby/LobbyRoomList.tsx
@@ -0,0 +1,12 @@
+import LobbyRoomListItem from "./LobbyRoomListItem";
+
+export default function LobbyRoomList() {
+  return (
+    <>
+      <div className="row">
+        <h3>전체 방 목록</h3>
+        <LobbyRoomListItem></LobbyRoomListItem>
+      </div>
+    </>
+  );
+}
---
+++ frontend_test/src/components/lobby/LobbyRoomSearchBar.tsx
@@ -0,0 +1,8 @@
+export default function LobbyRoomSearchBar() {
+  return (
+    <>
+      <input type="text" />
+      <button>검색</button>
+    </>
+  );
+}
