8413554ca15105048e3bba51420977ea4ba3e05e
snowwhitewolf <whdgus1029@naver.com>
2022-01-24 14:52:22 +0900

    [#S06P12D210-46] feat : :sparkles: 방리스트 더미데이터 생성

--- frontend_test/src/features/index.ts
+++ frontend_test/src/features/index.ts
@@ -17,7 +17,16 @@ export interface RoomList {
   list: Room[];
 }
 const initialState: RoomList = {
-  list: [],
+  list: [
+    { id: "aaaaa", title: "방 제목1", start: false },
+    { id: "aaaba", title: "방 제목2", start: false },
+    { id: "aaaca", title: "방 제목3", start: false },
+    { id: "aaaDa", title: "방 제목4", start: false },
+    { id: "aaaea", title: "방 제목5", start: false },
+    { id: "aaafa", title: "방 제목6", start: false },
+    { id: "aaaga", title: "방 제목7", start: false },
+    { id: "aaawa", title: "방 제목8", start: false },
+  ],
 };
 
 const actionPrefix = "ROOMS";
