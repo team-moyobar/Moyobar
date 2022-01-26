20273b3b21b8ecb8a26615a9b46c7ee87e0a8711
pondsuyeon <pondsuyeon@gmail.com>
2022-01-26 19:46:19 +0900

    [#S06P12D210-52] feat : :sparkles: 모임 목록 조회 수정
    - room 정보의 방 주인 이메일 -> 닉네임 형식 변경
    - 목록 조회 조건 추가

--- backend/src/main/java/com/ssafy/db/repository/HistoryRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/HistoryRepository.java
@@ -1,8 +1,9 @@
 package com.ssafy.db.repository;
 
+import com.ssafy.db.entity.ActionType;
 import com.ssafy.db.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
-    boolean existsByUserIdAndAction(long userId, String join);
+    boolean existsByUserIdAndAction(long userId, ActionType actionType);
 }
--- backend/src/main/java/com/ssafy/db/repository/RoomRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/RoomRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.db.repository;
 
 import com.ssafy.db.entity.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,6 @@ import java.util.Optional;
 public interface RoomRepository  extends JpaRepository<Room, Long > {
 
     Optional<Room> findRoomById(long roomId);
+
+    Page<Room> findAllByIsActive(int isActive, Pageable pageable);
 }
