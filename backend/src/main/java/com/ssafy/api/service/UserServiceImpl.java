64a225f04e59508271e7b4e091d0a7ac4109fe1b
지수연 <zzzxxzxz@naver.com>
2022-01-27 11:36:17 +0900

    [#S06P12D210-33] fix : :bug: 회원가입 주량 정보 버그 수정
    - 주량 제공 시 기본값 설정 안되는 버그 수정.

--- backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -40,8 +40,8 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
-	@Autowired
-	DrinkRepository drinkRepository;
+    @Autowired
+    DrinkRepository drinkRepository;
 
     @Autowired
     UserRepositorySupport userRepositorySupport;
@@ -52,118 +52,119 @@ public class UserServiceImpl implements UserService {
     // 온라인 유저 리스트
     ArrayList<String> userOnlineList = new ArrayList<>();
 
-	@Override
-	public User createUser(UserRegisterPostReq userRegisterInfo) {
-		User user = new User();
-		user.setUserId(userRegisterInfo.getUserId());
-		// 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
-		if(userRegisterInfo.getType() == ProviderType.LOCAL)
-			user.setPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
-		user.setNickname(userRegisterInfo.getNickname());
-		user.setBirthday(userRegisterInfo.getBirthday());
-		user.setPhone(userRegisterInfo.getPhone());
-		user.setType(userRegisterInfo.getType());
+    @Override
+    public User createUser(UserRegisterPostReq userRegisterInfo) {
+        User user = new User();
+        user.setUserId(userRegisterInfo.getUserId());
+        // 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
+        if (userRegisterInfo.getType() == ProviderType.LOCAL)
+            user.setPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
+        user.setNickname(userRegisterInfo.getNickname());
+        user.setBirthday(userRegisterInfo.getBirthday());
+        user.setPhone(userRegisterInfo.getPhone());
+        user.setType(userRegisterInfo.getType());
 
-		Drink drink = new Drink();
-		drink.setBeer(userRegisterInfo.getDrink().getBeer());
-		drink.setSoju(userRegisterInfo.getDrink().getSoju());
-		drink.setLiquor(userRegisterInfo.getDrink().getLiquor());
-		user.setDrink(drink);
-		return userRepository.save(user);
-	}
+        Drink drink = new Drink();
+        if (userRegisterInfo.getDrink() != null) {
+            drink.setBeer(userRegisterInfo.getDrink().getBeer());
+            drink.setSoju(userRegisterInfo.getDrink().getSoju());
+            drink.setLiquor(userRegisterInfo.getDrink().getLiquor());
+        }
+        user.setDrink(drink);
+        return userRepository.save(user);
+    }
 
-	@Override
-	public User updateUser(User user) {
-		return userRepository.save(user);
-	}
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
 
-	@Override
-	public User updateUser(UserUpdatePutReq userUpdatePutReq, User user) {
-		//닉네임 수정
-		if(userUpdatePutReq.getNickname() != null) {
-			user.setNickname(userUpdatePutReq.getNickname());
-		}
+    @Override
+    public User updateUser(UserUpdatePutReq userUpdatePutReq, User user) {
+        //닉네임 수정
+        if (userUpdatePutReq.getNickname() != null) {
+            user.setNickname(userUpdatePutReq.getNickname());
+        }
 
-		//이미지 수정
-		if(userUpdatePutReq.getImg() != null) {
-			user.setImg(userUpdatePutReq.getImg());
-		}
+        //이미지 수정
+        if (userUpdatePutReq.getImg() != null) {
+            user.setImg(userUpdatePutReq.getImg());
+        }
 
-		//주량정보 수정
-		if(userUpdatePutReq.getDrink() != null) {
-			//현재 유저의 drink값을 찾아 해당 유저의 Drink table값 수정하기
-			Drink drink = user.getDrink();
-			drink.setBeer(userUpdatePutReq.getDrink().getBeer());
-			drink.setSoju(userUpdatePutReq.getDrink().getSoju());
-			drink.setLiquor(userUpdatePutReq.getDrink().getLiquor());
-			drinkRepository.save(drink); //Drink table update 후에
-			user.setDrink(drink); //User table에 반영
-		}
+        //주량정보 수정
+        if (userUpdatePutReq.getDrink() != null) {
+            //현재 유저의 drink값을 찾아 해당 유저의 Drink table값 수정하기
+            Drink drink = user.getDrink();
+            drink.setBeer(userUpdatePutReq.getDrink().getBeer());
+            drink.setSoju(userUpdatePutReq.getDrink().getSoju());
+            drink.setLiquor(userUpdatePutReq.getDrink().getLiquor());
+            drinkRepository.save(drink); //Drink table update 후에
+            user.setDrink(drink); //User table에 반영
+        }
 
-		return userRepository.save(user);
-	}
+        return userRepository.save(user);
+    }
 
-	@Override
-	public boolean idDuplicated(String userId) {
-		return userRepository.existsByUserId(userId);
-	}
+    @Override
+    public boolean idDuplicated(String userId) {
+        return userRepository.existsByUserId(userId);
+    }
 
-	@Override
-	public boolean nicknameDuplicated(String nickname) {
-		return userRepository.existsByNickname(nickname);
-	}
+    @Override
+    public boolean nicknameDuplicated(String nickname) {
+        return userRepository.existsByNickname(nickname);
+    }
 
-	@Override
-	public void addUserOnline(String userId) {
-		// 온라인 유저 추가
-		userOnlineList.add(userId);
-	}
+    @Override
+    public void addUserOnline(String userId) {
+        // 온라인 유저 추가
+        userOnlineList.add(userId);
+    }
 
-	@Override
-	public List<UserRes> getUsersOnlineList() {
-		List<UserRes> res = new ArrayList<>();
-		// DB에서 유저 ID에 해당하는 유저리스트를 불러옴
-		for(String userId : userOnlineList){
-			res.add(UserRes.of(userRepository.findByUserId(userId).get()));
-		}
+    @Override
+    public List<UserRes> getUsersOnlineList() {
+        List<UserRes> res = new ArrayList<>();
+        // DB에서 유저 ID에 해당하는 유저리스트를 불러옴
+        for (String userId : userOnlineList) {
+            res.add(UserRes.of(userRepository.findByUserId(userId).get()));
+        }
 
-		return res;
-	}
+        return res;
+    }
 
-	@Override
-   public User getUserByUserId(String userId) {
-      // 디비에 유저 정보 조회 (userId 를 통한 조회).
-      User user = userRepository.findByUserId(userId).get();
-      return user;
-   }
+    @Override
+    public User getUserByUserId(String userId) {
+        // 디비에 유저 정보 조회 (userId 를 통한 조회).
+        User user = userRepository.findByUserId(userId).get();
+        return user;
+    }
 
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findByUserId(email)
-				.orElseThrow(() -> new UsernameNotFoundException("User not found with email : " + email));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByUserId(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email : " + email));
 
-		log.info("Load user by user email: the User Email - {}",  user.getUserId());
+        log.info("Load user by user email: the User Email - {}", user.getUserId());
 
-		return UserPrincipal.create(user);
-	}
+        return UserPrincipal.create(user);
+    }
 
-	@Override
-	public boolean changeUserPwd(String userId, String password, String newpassword)
-	{
-		User user = userRepository.findByUserId(userId).get();
+    @Override
+    public boolean changeUserPwd(String userId, String password, String newpassword) {
+        User user = userRepository.findByUserId(userId).get();
 
-		//현재 비밀번호를 받아와 DB에 저장된 내용과 일치하는지를 확인한 후에,
-		boolean check = passwordEncoder.matches(password, user.getPassword());
+        //현재 비밀번호를 받아와 DB에 저장된 내용과 일치하는지를 확인한 후에,
+        boolean check = passwordEncoder.matches(password, user.getPassword());
 
-		// 해당 유저의 비밀번호를 새 비밀번호값으로 변경하여 저장(암호화 후에)
-		if(check) {
-			user.setPassword(passwordEncoder.encode(newpassword));
-			userRepository.save(user);
-		}
-		return check;
-	}
+        // 해당 유저의 비밀번호를 새 비밀번호값으로 변경하여 저장(암호화 후에)
+        if (check) {
+            user.setPassword(passwordEncoder.encode(newpassword));
+            userRepository.save(user);
+        }
+        return check;
+    }
 
-	@Override
-	public User getUserByNickname(String nickname) {
-		return userRepository.findByNickname(nickname).orElseThrow(UserNotFoundException::new);
-	}
+    @Override
+    public User getUserByNickname(String nickname) {
+        return userRepository.findByNickname(nickname).orElseThrow(UserNotFoundException::new);
+    }
 }
