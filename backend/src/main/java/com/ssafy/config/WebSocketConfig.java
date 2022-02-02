1b2c5b136396923612b554edc2b18d37a9cd67b4
sanghyun-lee2 <h983019@naver.com>
2022-02-02 22:50:33 +0900

    [#S06P12D210-67] feat : :sparkles: Stomp 웹소켓 기능 수정  - Liar 경로를 통해 접속하도록 수정

--- backend/src/main/java/com/ssafy/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -10,11 +10,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+    @Override public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic/");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/game").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
