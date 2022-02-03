b1fd2f3e1880e643760e7036685e9add9b00d03a
sanghyun-lee2 <h983019@naver.com>
2022-02-03 17:14:30 +0900

    [#S06P12D210-83] feat : :sparkles: stomp 웹소켓 추가

--- frontend_test/src/routes/room/Room.tsx
+++ frontend_test/src/routes/room/Room.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { useParams } from "react-router";
+import Liar from "../../components/liar/Liar.js";
 
 interface ParamTypes {
-    roomId: string
+  roomId: string;
 }
-
 
 function Room() {
-    const { roomId } = useParams<ParamTypes>();
-    return (
-        <div>
-            <h1>방입장하셨습니다.</h1>
-            <p>{roomId}번 방</p>
-        </div>
-    )
+  const { roomId } = useParams<ParamTypes>();
+  return (
+    <div>
+      <Liar></Liar>
+      <h1>방입장하셨습니다.</h1>
+      <p>{roomId}번 방</p>
+    </div>
+  );
 }
 
-export default Room
+export default Room;
