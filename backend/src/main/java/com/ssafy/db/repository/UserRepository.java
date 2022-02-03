0f49d62a9acf0f297b84a4d6654760f357104674
지수연 <zzzxxzxz@naver.com>
2022-02-03 14:06:51 +0900

    [#S06P12D210-78] feat : :sparkles: 방 목록 조회 검색 조건 추가

--- backend/src/main/java/com/ssafy/db/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNickname(String nickname);
     boolean existsByUserId(String userId);
     boolean existsByNickname(String nickname);
+
+    List<User> findByNicknameContainingIgnoreCase(String keyword);
 }
