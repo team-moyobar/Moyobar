2dcd54661d6d4d45a6c618ba4ba77e780e90c6d8
chldudqlsdl <chldudqlsdl@naver.com>
2022-02-03 13:17:26 +0900

    [#S06P12D210-74] feat : :sparkles: 로비 페이지네이션 카운트 변경

---
+++ frontend_test/src/components/lobby/LobbyRoomOrder.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function LobbyRoomOrder() {
+  return (
+    <div>
+      <Button color="secondary"variant='contained'>최신순</Button>
+    </div>
+  );
+};
+
