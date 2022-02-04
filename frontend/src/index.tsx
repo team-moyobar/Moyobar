033fe5f26f85408111718119850962034edb74ef
snowwhitewolf <whdgus1029@naver.com>
2022-02-04 14:52:30 +0900

    [#S06P12D210-60] feat : :sparkles: 로그인 구현

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://i6d210.p.ssafy.io/api/v1";
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(
   <React.StrictMode>
--- frontend/src/routes/auth/home.tsx
+++ frontend/src/routes/auth/home.tsx
@@ -1,14 +1,20 @@
 import "./home.css";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
+  const history = useHistory();
+
+  function routeLogin() {
+    history.push("/login");
+  }
+
   return (
     <div className="home-container">
       <div className="home-box">
         <h1>MOYOBAR</h1>
-        <Link to="/login">
-          <button className="home-button">입장하기</button>
-        </Link>
+        <button onClick={routeLogin} className="home-button">
+          입장하기
+        </button>
       </div>
     </div>
   );
