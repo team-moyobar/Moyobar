92fe54a03fdb52e80448f285c0c2d60de2440746
snowwhitewolf <whdgus1029@naver.com>
2022-02-04 12:12:04 +0900

    [#S06P12D210-30] feat : :sparkles: 홈화면

--- frontend/src/routes/auth/Login.tsx
+++ frontend/src/routes/auth/Login.tsx
@@ -1,5 +1,11 @@
 import "./Login.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-container">
+      sdfsdf
+      <div className="login-left">sdfsdf</div>
+      <div className="login-right">sdfsdf</div>
+    </div>
+  );
 }
