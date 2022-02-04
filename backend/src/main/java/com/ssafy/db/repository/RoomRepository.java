35ec25e91e7665006aa75cbbc76a6d71bf4c078b
pondsuyeon <pondsuyeon@gmail.com>
2022-02-04 16:56:06 +0900

    [#S06P12D210-78] feat : :sparkles: 방 전체 키워드 검색 추가
    - 검색 조건 searchBy all 추가했습니다.

--- backend/src/main/java/com/ssafy/db/repository/RoomRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/RoomRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface RoomRepository  extends JpaRepository<Room, Long > {
+public interface RoomRepository extends JpaRepository<Room, Long> {
 
     Optional<Room> findRoomById(long roomId);
 
@@ -20,4 +20,6 @@ public interface RoomRepository  extends JpaRepository<Room, Long > {
     Page<Room> findAllByIsActiveAndOwnerIn(int isActive, List<User> users, Pageable pageable);
 
     Page<Room> findAllByIsActiveAndDescriptionContainingIgnoreCase(int isActive, String keyword, Pageable pageable);
+
+    Page<Room> findAllByDescriptionContainingIgnoreCaseOrTitleContainingIgnoreCaseOrOwnerInAndIsActive(String keyword1, String keyword2, List<User> users, int isActive, Pageable pageable);
 }
