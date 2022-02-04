51c2a52620befe876d22b8ab4740cf8f6e9aeb02
snowwhitewolf <whdgus1029@naver.com>
2022-02-04 14:52:30 +0900

    [#S06P12D210-60] feat : :sparkles: 로그인 구현

--- frontend/src/routes/auth/Profile.tsx
+++ frontend/src/routes/auth/Profile.tsx
@@ -1,5 +1,12 @@
 import "./Profile.css";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return <div className="profile-container">
+    <div className="profile-menu">
+
+    </div>
+    <div className="profile-content">
+      
+    </div>
+  </div>;
 }
