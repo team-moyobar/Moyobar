e85886275793ca44e363ffc879d11f3b975ee01f
chldudqlsdl <chldudqlsdl@naver.com>
2022-02-06 23:48:20 +0900

    style : :art: 로비 화면 다크모드 변경

--- frontend_test/src/components/lobby/LobbyPagination.tsx
+++ frontend_test/src/components/lobby/LobbyPagination.tsx
@@ -2,24 +2,17 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box'; 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export default function LobbyPagination({onChange, totalPages}: any) {
   
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     onChange(value);}
   
-  const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  });
+
 
   return (
-      <ThemeProvider theme={darkTheme}>
-        <Stack spacing={8} >
-          <Pagination count={totalPages} color="secondary" onChange={handleChange} variant='outlined' />
-        </Stack>
-      </ThemeProvider>
+    <Stack spacing={8} >
+      <Pagination count={totalPages} color="secondary" onChange={handleChange} variant='outlined' />
+    </Stack>
   );
 }
