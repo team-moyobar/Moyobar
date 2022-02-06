7a362579e14e6526620b112afa5631ec799d743c
p9a3r5k0 <p9a3r5k0@naver.com>
2022-02-06 23:54:24 +0900

    [#S06P12D210-89] feat : :spakles: 방 채팅 기능 구현

---
+++ frontend/src/components/room/Message.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Username = styled.p`
+  color: #42387a;
+  font-size: 0.8rem;
+  font-weight: 600;
+  padding: 5px 0 10px 0;
+`;
+
+const MessageContainer = styled.div`
+  width: 100px;
+`;
+
+const Text = styled.p`
+  font-size: 1rem;
+`;
+
+class Message extends Component {
+  render() {
+    const { text, userName } = this.props;
+
+    return (
+      <MessageContainer>
+        <Username>{userName}</Username>
+        <Text>{text}</Text>
+      </MessageContainer>
+    );
+  }
+}
+
+export default Message;
