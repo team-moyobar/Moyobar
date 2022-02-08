b03d0cc7e28b0b9ff244ba72bf4d9990c8e9077a
sanghyun-lee2 <h983019@naver.com>
2022-02-08 22:09:30 +0900

    [#S06P12D210-96] feat : :sparkles: 업다운게임 화면 추가

--- frontend/src/components/room/GameSelect.tsx
+++ frontend/src/components/room/GameSelect.tsx
@@ -1,51 +1,57 @@
-import React from 'react';
-import StompLiar from '../game/Liar'
-import Updown from '../game/Updown';
-import { useState } from 'react';
-import Button from '@mui/material/Button';
+import React from "react";
+import StompLiar from "../game/Liar";
+import StompUpdown from "../game/Updown";
+import { useState } from "react";
+import Button from "@mui/material/Button";
 
 const GameSelect = () => {
-
-  const [gameSelected, setGameSelected] = useState(false)
-  const [liar, setLiar] = useState(false)
-  const [upDown, setUpDown] = useState(false)
+  const [gameSelected, setGameSelected] = useState(false);
+  const [liar, setLiar] = useState(false);
+  const [upDown, setUpDown] = useState(false);
 
   const handleLiar = () => {
-    setLiar(true)
-    setGameSelected(true)
-  }
+    setLiar(true);
+    setGameSelected(true);
+  };
   const handleUpDown = () => {
-    setUpDown(true)
-    setGameSelected(true)
-  }
+    setUpDown(true);
+    setGameSelected(true);
+  };
   const handleGameChange = () => {
-    setGameSelected(false)
-    setUpDown(false)
-    setLiar(false)
-  }
-
+    setGameSelected(false);
+    setUpDown(false);
+    setLiar(false);
+  };
 
   return (
     <div>
       {gameSelected == false ? (
         <div>
           <h2>게임을 골라주세요</h2>
-          <Button variant="contained" onClick={handleLiar}>라이어게임</Button>
-          <Button variant="contained" onClick={handleUpDown}>업다운게임</Button>
+          <Button variant="contained" onClick={handleLiar}>
+            라이어게임
+          </Button>
+          <Button variant="contained" onClick={handleUpDown}>
+            업다운게임
+          </Button>
         </div>
-        ) : null}
+      ) : null}
       {liar == true ? (
         <div>
           <StompLiar></StompLiar>
-          <Button variant="contained" color="error" onClick={handleGameChange}>다른 게임 선택하기</Button>
+          <Button variant="contained" color="error" onClick={handleGameChange}>
+            다른 게임 선택하기
+          </Button>
         </div>
-      ) : null } 
-      {/* {upDown == true ? (
+      ) : null}
+      {upDown == true ? (
         <div>
-          <Updown></Updown>
-          <Button variant="contained" color="error" onClick={handleGameChange}>다른 게임 선택하기</Button>
+          <StompUpdown></StompUpdown>
+          <Button variant="contained" color="error" onClick={handleGameChange}>
+            다른 게임 선택하기
+          </Button>
         </div>
-      ) : null }  */}
+      ) : null}
     </div>
   );
 };
