f213935454290f1d3e5de6c97a990175a8034945
sanghyun-lee2 <h983019@naver.com>
2022-02-08 11:52:13 +0900

    [#S06P12D210-90] feat : :sparkles: 라이어, 업다운 게임 추가

---
+++ frontend/src/components/game/VoteDlg.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import PersonIcon from "@mui/icons-material/Person";
+import Typography from "@mui/material/Typography";
+import { blue } from "@mui/material/colors";
+
+export interface SimpleDialogProps {
+  open: boolean;
+  selectedValue: string;
+  users: string[];
+  onClose: (value: string) => void;
+}
+
+export function VoteDlg(props: SimpleDialogProps) {
+  const { onClose, selectedValue, open, users } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value: string) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>라이어를 선택해주세요</DialogTitle>
+      <List sx={{ pt: 0 }}>
+        {users.map((email) => (
+          <ListItem
+            button
+            onClick={() => handleListItemClick(email)}
+            key={email}
+          >
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                <PersonIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={email} />
+          </ListItem>
+        ))}
+      </List>
+    </Dialog>
+  );
+}
