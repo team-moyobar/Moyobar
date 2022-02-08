7531bbe3d2dd6cb045e6ce8712f2c3e79384dd27
지수연 <zzzxxzxz@naver.com>
2022-02-08 13:49:56 +0900

    [#S06P12D210-80] feat : :sparkles: UP Down Game 정보 DB 저장 구현

--- backend/src/main/java/com/ssafy/stomp/service/GameService.java
+++ backend/src/main/java/com/ssafy/stomp/service/GameService.java
@@ -1,9 +1,8 @@
 package com.ssafy.stomp.service;
 import com.ssafy.db.entity.Game;
-import com.ssafy.db.entity.GameCategory;
 import com.ssafy.db.entity.User;
 
 public interface GameService {
     Game createGame(String gameName);
-    Game updateGame(long gameId, User winner);
+    Game updateGame(long gameId, String winner);
 }
