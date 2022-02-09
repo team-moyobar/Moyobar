4b60e10924356ceffa4bcd051d452e64a31c1513
지수연 <zzzxxzxz@naver.com>
2022-02-09 14:56:15 +0900

    [#S06P12D210-98] feat : :sparkles: 우리말샘 API 요청
    - 우리말샘 API 이용해 결과값 받아오도록 구현

---
+++ backend/src/main/java/com/ssafy/common/service/ApiService.java
@@ -0,0 +1,37 @@
+package com.ssafy.common.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ApiService<T> {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public ResponseEntity<T> get(String url, HttpHeaders httpHeaders) {
+        return callApiEndpoint(url, HttpMethod.GET, httpHeaders, null, (Class<T>) Object.class);
+    }
+
+    public ResponseEntity<T> get(String url, HttpHeaders httpHeaders, Class<T> clazz) {
+        return callApiEndpoint(url, HttpMethod.GET, httpHeaders, null, clazz);
+    }
+
+    public ResponseEntity<T> post(String url, HttpHeaders httpHeaders, Object body) {
+        return callApiEndpoint(url, HttpMethod.POST, httpHeaders, body, (Class<T>) Object.class);
+    }
+
+    public ResponseEntity<T> post(String url, HttpHeaders httpHeaders, Object body, Class<T> clazz) {
+        return callApiEndpoint(url, HttpMethod.POST, httpHeaders, body, clazz);
+    }
+
+
+    private ResponseEntity<T> callApiEndpoint(String url, HttpMethod httpMethod, HttpHeaders httpHeaders, Object body, Class<T> clazz) {
+        return restTemplate.exchange(url, httpMethod, new HttpEntity<>(body, httpHeaders), clazz);
+    }
+}
