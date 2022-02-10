b651d138a7fede4520e69efb9927219a57210e2a
chldudqlsdl <chldudqlsdl@naver.com>
2022-02-10 18:45:10 +0900

    style : :art: 프로필 페이지 디자인

--- frontend/src/components/auth/ProfileUserInfo.tsx
+++ frontend/src/components/auth/ProfileUserInfo.tsx
@@ -1,19 +1,44 @@
 import { UserInfo } from "../../routes/auth/Profile";
+import TextField from '@mui/material/TextField';
+import "./ProfileUserInfo.css"
 
 
 const ProfileUserInfo = (props: { user : UserInfo }) => {
 
   const user = props.user
+  let query;
+  if (user.drink.soju !== 0) {
+    query = "소주";
+  } else if (user.drink.beer !== 0) {
+    query = "맥주";
+  } else if (user.drink.liquor !== 0) {
+    query = "양주";
+  }
+  console.log(user.img)
   
   return (
-    <div>
-      <h1>프로필</h1>
-      <p>1. {user.user_id}</p>
-      <p>2. {user.nickname}</p>
-      <p>3. {user.birthday}</p>
+    <div className="profile-userinfo-contents">
+      <div className="profile-picture">
+        <div className="profile-picture-img">
+          <img src="/images/profile1.jfif" alt="" />
+        </div>
+      </div>
+      <div className="profile-item">
+        <span className="profile-item-title">닉네임</span>
+        <span>{user.nickname}</span>
+      </div>
+      <div className="profile-item">
+        <span className="profile-item-title">생년월일</span>
+        <span>{user.birthday}</span>
+      </div>
+      <div className="profile-item">
+        <span className="profile-item-title">주량</span>
+        <span>{query} {user.drink.soju} 잔</span>
+      </div>
+      {/* <p>1. {user.user_id}</p>
       <p>4. {user.img}</p>
       <p>5. {user.score}</p>
-      <p>6. {user.drink.beer}</p>
+      <p>6. {user.drink.beer}</p> */}
     </div>
   )
 };
