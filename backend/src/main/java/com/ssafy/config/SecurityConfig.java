a01ed416030efebb97062735a9ec31096adf1e95
laura707 <laura707@naver.com>
2022-02-13 16:06:19 +0900

    [#S06P12D210-107] fix : :bug: 소셜로그인,회원가입 버그 수정
    - 소셜 로그인, 회원가입 로직을 모여바 서비스에 맞게 수정
    - 배포 주소에 맞게 uri 변경
    - oauth redirect uri 및 redirect_uri 재설정, 클라이언트 아이디-시크릿값 변경

--- backend/src/main/java/com/ssafy/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.config;
 
+import com.ssafy.api.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,12 +28,15 @@ import com.ssafy.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.ssafy.security.oauth2.handler.OAuth2AuthenticationSuccessHandler;
 import com.ssafy.security.oauth2.handler.OAuth2AuthenticationFailureHandler;
 
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
 /**
  * 인증(authentication) 와 인가(authorization) 처리를 위한 스프링 시큐리티 설정 정의.
  */
 @Configuration
-@EnableWebSecurity //Spring Security 활성화
-@EnableGlobalMethodSecurity(prePostEnabled = true) //AOP 보안 제공. 스프링 시큐리티의 메소드 어노테이션 기반 시큐리티를 활성화 하기 위해서 필요
+@EnableWebSecurity //Spring Security 활성화-스프링 시큐리티 필터가 스프링 필터체인에 등록됨
+@EnableGlobalMethodSecurity(prePostEnabled = true) //AOP 보안 제공. 스프링 시큐리티의 메소드 어노테이션 기반 시큐리티를 활성화 하기 위해서 필요. preAuthorization활성화
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -48,12 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
-
-    //로그인시 JWT Token을 확인해 인가된 사용자 유무를 판별하고 내부 process를 수행 -> JwtAuthenticationFilter 하나만 사용하도록 주석
-//    @Bean
-//    public TokenAuthenticationFilter tokenAuthenticationFilter() {
-//        return new TokenAuthenticationFilter();
-//    }
 
     //쿠키에 Authorization request 정보 저장
     @Bean
@@ -79,7 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     //DAO 기반의 Authentication Provider가 적용되도록 설정
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.authenticationProvider(authenticationProvider());
     }
 
@@ -88,8 +86,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .cors() //cors를 허용
                 .and()
-                .httpBasic().disable()
-                .csrf().disable() //"/"와 "/main" 경로는 누구나 접근가능하나 그외의 경로는 인증허가시에만 접근 가능
+                .httpBasic().disable() //헤더에 basic oauth값이 오는데 이를 막음
+                .csrf().disable() //"/"와 "/main" 경로는 누구나 접근가능하나 그외의 경로는 인증허가시에만 접근 가능. csrf 토큰 받지 않음
 
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증(세션사용X)
                 .and()
@@ -110,22 +108,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest() //그 외의 request에 대해서는
                 .permitAll() //모두 접근 가능
 
-                 //Authorization Server
+                //Authorization Server
                 .and()
                 .oauth2Login() //로그인 창에서 로그인 및 사용자 동의 얻은 후
                 .authorizationEndpoint() //인증 요청 받으면
-                .baseUri("/oauth2/authorization") //해당 url(provider별 AuthorizationURl)로 redirect(클라이언트에서 처음 로그인 시도하는 URI)
+                .baseUri("/oauth2/authorization") //해당 url(provider별 AuthorizationURl)로 redirect
                 .authorizationRequestRepository(cookieAuthorizationRequestRepository()) //authorization request 관련 state 저장
 
-                 //Resource Server
+                //Resource Server
                 .and()
                 .redirectionEndpoint() //아래 url로 redirect
                 .baseUri("/*/oauth2/code/*") //authorization code(resource server 접근용) 갖고 resource server로 이동
 
-                //받아온 User정보를 저장
+                //받아온 User 정보를 통해 회원정보 조회
                 .and()
                 .userInfoEndpoint()
-                .userService(customOAuth2UserService) //커스텀한 소셜로그인 로직 처리용 클래스
+                .userService(customOAuth2UserService) //커스텀한 소셜로그인 로직 처리용 클래스-회원가입하기 or 로그인하기
 
                 .and()
                 .successHandler(oAuth2AuthenticationSuccessHandler) //jwt authentication token 생성
@@ -133,5 +131,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Add our custom Token based authentication filter
         http.addFilterBefore(new JwtAuthenticationFilter(authenticationManager(), userService), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    //소셜 로그인 JSESSION 발급 시 요청 URI에 ; 들어가도 허용하도록 변경
+    @Bean
+    public HttpFirewall getHttpFirewall() {
+        StrictHttpFirewall strictHttpFirewall = new StrictHttpFirewall();
+        strictHttpFirewall.setAllowSemicolon(true);
+        return strictHttpFirewall;
     }
 }
