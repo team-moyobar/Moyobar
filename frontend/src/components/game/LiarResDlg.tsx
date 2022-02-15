5465966a1bf5e5f84998760b7efb11131a9276da
sanghyun-lee2 <h983019@naver.com>
2022-02-15 15:35:15 +0900

    [#S06P12D210-84] fix : :bug: 라이어 게임 버그 수정

---
+++ frontend/src/components/game/LiarResDlg.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect } from "react";
+import Dialog from "@mui/material/Dialog";
+
+import "./LiarResDlg.css";
+
+export interface LiarResDlgProps {
+  open: boolean;
+  onClose: () => void;
+  liar: string;
+  voteRes: voteResultObj[];
+  winner: string;
+}
+
+// 라이어 게임결과 인터페이스
+export interface gameResultObj {
+  liar: string;
+  voteresult: voteResultObj[];
+  winner: string;
+}
+
+// 라이어 투표결과 인터페이스
+export interface voteResultObj {
+  nickname: string;
+  votecnt: string;
+}
+
+// const isFullWidth: boolean = true;
+
+export function LiarResDlg(props: LiarResDlgProps) {
+  const { open, onClose, liar, voteRes, winner } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  useEffect(() => {
+    let timer: number = 0;
+    if (open === true) {
+      timer = window.setTimeout(() => {
+        handleClose();
+      }, 7000);
+    }
+
+    return () => clearInterval(timer);
+  }, [open]);
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      open={open}
+      //   fullWidth={isFullWidth}
+      PaperProps={{
+        style: {
+          //backgroundImage: `url("/images/room/cheers.jpg")`,
+          backgroundColor: "grey",
+          minWidth: "40vh",
+          maxWidth: "40vh",
+          minHeight: "60vh",
+          maxHeight: "60vh",
+        },
+      }}
+    >
+      <div className="liarresdlg-container">
+        <h2>게임결과</h2>
+        <div>
+          <h3>라이어 : [{liar}]</h3>
+        </div>
+        <h2>투표결과</h2>
+        <ul>
+          {voteRes.map((voteres) => (
+            <li key={voteres.nickname}>
+              {voteres.nickname} : {voteres.votecnt} 표
+            </li>
+          ))}
+        </ul>
+        {winner === "liar" && (
+          <p>
+            라이어 <span>승리</span>
+          </p>
+        )}
+        {winner !== "liar" && (
+          <p>
+            라이어 <span>패배</span>
+          </p>
+        )}
+      </div>
+    </Dialog>
+  );
+}
